464cbd7841f743694acd8b675e0d637755922131
Viktor Kotseruba <barbuzaster@gmail.com>
2013-07-18 21:46:42 +0800

    expm package spec

---
+++ package.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "eidetic", description: "alternate mnesia interface for elixir",
+                 version: "0.0.1", keywords: ["Elixir", "mnesia"], 
+                 maintainers: [[name: "Victor Kotseruba", 
+                                email: "barbuzaster@gmail.com"]],
+                 repositories: [[github: "barbuza/eidetic"]])
