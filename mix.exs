2c448d079104773808acf826780fc6848b6ff41a
Viktor Kotseruba <barbuzaster@gmail.com>
2013-07-18 21:45:53 +0800

    elixir version ~> 0.10

--- mix.exs
+++ mix.exs
@@ -4,18 +4,10 @@ defmodule Eidetic.Mixfile do
   def project do
     [ app: :eidetic,
       version: "0.0.1",
-      elixir: "~> 0.10.1-dev",
-      deps: deps ]
+      elixir: "~> 0.10",
+      deps: [] ]
   end
 
-  # Configuration for the OTP application
-  def application do
-    []
-  end
+  def application, do: []
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
-  defp deps do
-    []
-  end
 end
